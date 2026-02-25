2dce14cfaf115d91056572682845b122b7b6d632
Gabriel Lindsey <gabe@gabriellindsey.com>
2026-02-25 11:29:51 -0600

    Setting SSH Port via Config now

--- scripts/backup/sync_nextcloud.sh
+++ scripts/backup/sync_nextcloud.sh
@@ -11,5 +11,8 @@ SQL_OUT="/mnt/Backups/nextcloud/sql/nextcloud_${STAMP}.sql"
 CONF_PATH="/mnt/Backups/nextcloud/nextcloud.conf"
 [ -f "$CONF_PATH" ] && source "$CONF_PATH"
 
-rsync -aHAX --delete -e ssh root@${NC_PRI}:${NC_DATA_SRC}/ /mnt/Backups/nextcloud/data/
-ssh root@${NC_PRI} "mysqldump --single-transaction -u${DB_USER} -p'${DB_PASS}' ${DB_NAME}" > ${SQL_OUT}
+# Default SSH port if not set
+NC_SSH_PORT="${NC_SSH_PORT:-22}"
+
+rsync -aHAX --delete -e "ssh -p ${NC_SSH_PORT}" root@${NC_PRI}:${NC_DATA_SRC}/ /mnt/Backups/nextcloud/data/
+ssh -p ${NC_SSH_PORT} root@${NC_PRI} "mysqldump --single-transaction -u${DB_USER} -p'${DB_PASS}' ${DB_NAME}" > ${SQL_OUT}
