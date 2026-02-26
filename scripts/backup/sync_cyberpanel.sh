93f38663d08aed275741010dec19cee5c4c82ec8
Gabriel Lindsey <gabe@gabriellindsey.com>
2026-02-25 19:03:22 -0600

    Update sync_cyberpanel.sh

--- scripts/backup/sync_cyberpanel.sh
+++ scripts/backup/sync_cyberpanel.sh
@@ -8,6 +8,6 @@ flock -n 9 || exit 0
 CONF_PATH="/mnt/Backups/cyberpanel/cyberpanel.conf"
 [ -f "$CONF_PATH" ] && source "$CONF_PATH"
 
-rsync -aHAX --delete -e ssh root@${CP_PRI}:/home/ /mnt/Backups/cyberpanel/home/
+rsync -aHAX --delete -e ssh gabe@${CP_PRI}:/home/ /mnt/Backups/cyberpanel/home/
 STAMP=$(date +%F_%H%M%S)
-ssh root@${CP_PRI} "mysqldump --all-databases --single-transaction" | gzip > /mnt/Backups/cyberpanel/db/cyberpanel_${STAMP}.sql.gz
+ssh gabe@${CP_PRI} "mysqldump --all-databases --single-transaction" | gzip > /mnt/Backups/cyberpanel/db/cyberpanel_${STAMP}.sql.gz
