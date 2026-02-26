3062d69fdfffe94ceb9c74388cfa4fed12dcd9cd
Gabriel Lindsey <gabe@gabriellindsey.com>
2026-02-25 19:44:11 -0600

    Suppressing False Negative

--- provision.sh
+++ provision.sh
@@ -491,7 +491,7 @@ provision_cyberpanel() {
     step "Updating CyberPanel (safe upgrade, data preserved)"
     wget -O /tmp/cyberpanel_install.sh https://cyberpanel.net/install.sh
     # Suppress only 'Unknown argument...' and help text
-    bash /tmp/cyberpanel_install.sh --upgrade 2>&1 | awk '/CyberPanel Installer Script Help/ {exit} {print}'
+    bash /tmp/cyberpanel_install.sh --upgrade 2>&1 | awk '/Unknown argument/ {skip=1} /CyberPanel Installer Script Help/ {exit} !skip {print}'
     rm -f /tmp/cyberpanel_install.sh
     ok "CyberPanel updated (user data preserved)"
   else
