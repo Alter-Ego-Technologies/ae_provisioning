5c5bfa85534f478c64d5e16b1eeac819592774e2
Gabriel Lindsey <gabe@gabriellindsey.com>
2026-02-28 17:12:09 -0600

    Lock file

--- scripts/backup/sync_mailcow.sh
+++ scripts/backup/sync_mailcow.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -e
 mountpoint -q /mnt/Backups || { echo "ERROR: /mnt/Backups not mounted"; exit 1; }
-exec 9>/var/lock/backup.lock
+exec 9>/tmp/mailcow-backup.lock
 flock -n 9 || exit 0
-
-MC_PRI=10.0.0.12
-MC_BACKUP_SRC="/opt/mailcow-dockerized/backup"
 
 # Source config from /mnt/Backups/mailcow/mailcow.conf if present
 CONF_PATH="/mnt/Backups/mailcow/mailcow.conf"
 [ -f "$CONF_PATH" ] && source "$CONF_PATH"
 
-rsync -aHAX --delete -e ssh root@${MC_PRI}:${MC_BACKUP_SRC}/ /mnt/Backups/mailcow/backups/
+rsync -aHAX --no-group --delete -e "ssh -p ${MC_SSH_PORT}" ${MC_SSH_USER}@${MC_PRI}:${MC_BACKUP_SRC}/ ${MC_BACKUP_DST}/
